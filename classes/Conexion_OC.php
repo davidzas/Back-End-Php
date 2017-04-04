3760c9f7decb2176d32f56065a2cfb0ab7af7715
davidzast <dftobar@telebucaramanga.com.co>
2017-04-04 07:38:41 -0500

    update licence

--- classes/Conexion_OC.php
+++ classes/Conexion_OC.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 
-Copyright (c) 2016 David Tobar alias DavidZas
+Copyright (c) 2016 David Tobar aka DavidZas
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 and associated documentation files (the "Software"), to deal in the Software without restriction,
